20f5cba15cbd80535de1656203d89af02f8c6bf9
Sameer Akhter <akhtersameer736@gmail.com>
2025-03-18 15:14:28 +0530

    Update

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -48,17 +48,17 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 13611, hash: 'a5e288edcc7031be1364471e51a96923a7bf8a8a30097a8bbe894bf99394c9ac', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1195, hash: '481662216819fc971f29c9260ef1b11d20138d58a1478d8c8a344f3035b9861d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'about-us/index.html': {size: 52545, hash: 'e46780e20bfdd06931a6751e3b205f540e3f71bca029ecc029a8a0cc425c20dd', text: () => import('./assets-chunks/about-us_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 67079, hash: '10e2026c0620baa186e20a4fe24d247b450e6378b85e2338f1e96fe3c55e6a4e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'contact/index.html': {size: 52322, hash: '99101cedadcb64a91612d431399f9c62c2f650d25e0fbc0d52aa11865e574a13', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
-    'career/index.html': {size: 49765, hash: 'eb6eeef39e929907a26743db1b7717dfd680f6231c68750411c5ba726898d168', text: () => import('./assets-chunks/career_index_html.mjs').then(m => m.default)},
-    'digital-engineering/index.html': {size: 51310, hash: 'fd1e423aba4018d8edb8b427233b8d52ba49f4441fa285f72829a5e066fd8737', text: () => import('./assets-chunks/digital-engineering_index_html.mjs').then(m => m.default)},
-    'software-engineering/index.html': {size: 55172, hash: '8d5ee8fd6ea3a15620f1c1393c053d71dfea75ddaa82c645012f18763b19c739', text: () => import('./assets-chunks/software-engineering_index_html.mjs').then(m => m.default)},
-    'product-engineering/index.html': {size: 50873, hash: 'ad647f30fd2a938bf87d2ee423288a44a6f7a900c13b575c1d91b76140cafd1d', text: () => import('./assets-chunks/product-engineering_index_html.mjs').then(m => m.default)},
-    'industries/index.html': {size: 55454, hash: 'ec618176865222752c60d069e1428499cde735f553b05c313cdd6d9199a95050', text: () => import('./assets-chunks/industries_index_html.mjs').then(m => m.default)},
-    'consulting/index.html': {size: 51683, hash: 'ca72d1f1cb9c63343f015d73729a952969d0244ffca9e998331fea4175b18ca1', text: () => import('./assets-chunks/consulting_index_html.mjs').then(m => m.default)},
-    'styles-2UUXBMJI.css': {size: 237157, hash: '164Ih8QeP6k', text: () => import('./assets-chunks/styles-2UUXBMJI_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5830, hash: 'd91f25e70383c59e6ae6b7de1d47020966c362dbc38bf701d282dd248bb9c22e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1195, hash: '4a2d66783d0b6cd90eee9c56b2c5ab10dbdf5a1da08b88bfc09df0479a6873f0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'about-us/index.html': {size: 45161, hash: '315101ac2bbe04092710329f22e9959eb9998c6a2bf8a59117f948892c6eb27d', text: () => import('./assets-chunks/about-us_index_html.mjs').then(m => m.default)},
+    'contact/index.html': {size: 44938, hash: 'e3b0015c820aed2a9489830f63007c8f814e75c057b9cfd6e191f62d66a7c66a', text: () => import('./assets-chunks/contact_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 59695, hash: '909f4f88396cab79cfa82a7018156a2bcbe152b3c6577c8459a4f7e17e5feecd', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'career/index.html': {size: 42381, hash: '8fc10d37d69edd6d3325b99fec56df63df388bc7e7cb81fdff780ed2945066aa', text: () => import('./assets-chunks/career_index_html.mjs').then(m => m.default)},
+    'software-engineering/index.html': {size: 47788, hash: '84e1406ac79a3afdb59b7e3efeb8cc6b94e7a37cb15488c99c8177f2d8213cc8', text: () => import('./assets-chunks/software-engineering_index_html.mjs').then(m => m.default)},
+    'digital-engineering/index.html': {size: 43926, hash: '7910b4576428b5b58e0a48a84f5cff4f607f082f2b027383d54c5fc2794d1f0a', text: () => import('./assets-chunks/digital-engineering_index_html.mjs').then(m => m.default)},
+    'industries/index.html': {size: 48070, hash: 'dcdd27914c33ce09663cfd55e076deeaac6cde339e87b4405413ddefdab5332e', text: () => import('./assets-chunks/industries_index_html.mjs').then(m => m.default)},
+    'product-engineering/index.html': {size: 43489, hash: '7aaa503ee3edd7c68658a08ed31520ae34833de9bf03cbb9d444f1b0faf5ae69', text: () => import('./assets-chunks/product-engineering_index_html.mjs').then(m => m.default)},
+    'consulting/index.html': {size: 44299, hash: 'cb08557736d4b35bdacc5b612322092e4d263558a149b7a3c57d2d0755172813', text: () => import('./assets-chunks/consulting_index_html.mjs').then(m => m.default)},
+    'styles-TXAQJSFE.css': {size: 229376, hash: 'ucSHLaR3Xbw', text: () => import('./assets-chunks/styles-TXAQJSFE_css.mjs').then(m => m.default)}
   },
 };
